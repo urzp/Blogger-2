0e4a0586b6f760ae67164d5ce12ba31d304c92a7
urzp <urzp002@mail.ru>
2016-04-16 22:03:31 +0300

    Working with the Database: Modifying change

--- blogger/db/migrate/20160416185429_create_articles.rb
+++ blogger/db/migrate/20160416185429_create_articles.rb
@@ -1,8 +1,9 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
-
-      t.timestamps null: false
+        t.string :title
+        t.text :body
+        t.timestamps
     end
   end
 end
