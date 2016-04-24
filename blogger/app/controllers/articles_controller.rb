be3829be89b5f7b01a721b419c4eff5d3692ae18
urzp <urzp002@mail.ru>
2016-04-24 12:11:18 +0300

    Form-based Workflow Creating the NEW Action and View

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
     
+    def new
+        @article = Article.new    
+    end
+    
     def show
         @article = Article.find(params[:id])
     end
