4707f90091375b7442e74012fccdd640ff3be3ee
urzp <urzp002@mail.ru>
2016-04-24 12:51:19 +0300

    Create 2

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,9 +1,7 @@
 class ArticlesController < ApplicationController
     
     def create
-        @article = Article.new
-        @article.title = params[:article][:title]
-        @article.body= params[:article][:body]
+        @article = Article.new(article_params)
         @article.save
         redirect_to article_path(@article)
 
@@ -20,4 +18,12 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+    
+private
+    
+    def article_params
+        params.require(:article).permit(:title, :body)
+    end
+    
+    
 end
