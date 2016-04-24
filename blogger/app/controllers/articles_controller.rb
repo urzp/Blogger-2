7e78c9520dac405324e152d2849bd1ac56758685
urzp <urzp002@mail.ru>
2016-04-24 12:39:55 +0300

    Create 1

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,5 +1,14 @@
 class ArticlesController < ApplicationController
     
+    def create
+        @article = Article.new
+        @article.title = params[:article][:title]
+        @article.body= params[:article][:body]
+        @article.save
+        redirect_to article_path(@article)
+
+    end
+    
     def new
         @article = Article.new    
     end
